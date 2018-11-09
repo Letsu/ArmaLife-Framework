9833f28a1e8b78b3ba2db3e2324e32d1b058f85a
Marcel <MarcelReinecke@gmx.de>
2018-11-09 20:55:03 +0100

    First Upload Spawn Dialog

---
+++ dialogs/Spawn_Dialog.cpp
@@ -0,0 +1,36 @@
+class Backgrund: RscText
+{
+	idc = 1000;
+	x = 0.309219 * safezoneW + safezoneX;
+	y = 0.401 * safezoneH + safezoneY;
+	w = 0.381563 * safezoneW;
+	h = 0.187 * safezoneH;
+};
+class Listbox-Spawn-Selection: RscListbox
+{
+	idc = 1500;
+	x = 0.613438 * safezoneW + safezoneX;
+	y = 0.412 * safezoneH + safezoneY;
+	w = 0.0721875 * safezoneW;
+	h = 0.088 * safezoneH;
+	tooltip = "Auswahl deines Spawn punktes"; //--- ToDo: Localize;
+};
+class Button-Spawn: RscButton
+{
+	idc = 1600;
+	text = "Spawn"; //--- ToDo: Localize;
+	x = 0.613437 * safezoneW + safezoneX;
+	y = 0.522 * safezoneH + safezoneY;
+	w = 0.0721875 * safezoneW;
+	h = 0.055 * safezoneH;
+	tooltip = "Zum Spawnen den Button DrÃ¼cken"; //--- ToDo: Localize;
+};
+class Text-Map: RscText
+{
+	idc = 1001;
+	x = 0.319531 * safezoneW + safezoneX;
+	y = 0.412 * safezoneH + safezoneY;
+	w = 0.283594 * safezoneW;
+	h = 0.165 * safezoneH;
+	tooltip = "die Karte"; //--- ToDo: Localize;
+};
