531060991318fbb84269e40dd791d0551bac6740
Marcel <MarcelReinecke@gmx.de>
2018-11-10 13:59:52 +0100

    First Vars for Licenses

--- config/Config_licence.cpp
+++ config/Config_licence.cpp
@@ -3,21 +3,18 @@ class Config_licence
       class Driver_licence_A
       {
           DisplayName = "Führerschein Klasse A";
-
+          Licens_1 = Driver_licence_A
       };
 
       class Driver_licence_B
       {
           DisplayName = "Führerschein Klasse B";
-
+          Licens_2 = Driver_licence_B
       };
 
       class Driver_licence_C
       {
           DisplayName = "Führerschein Klasse C";
-
+          Licens_3 = Driver_licence_C
       };
-
-
-
   };
