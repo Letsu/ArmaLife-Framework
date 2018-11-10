e8e8bc44d35b01109dab902272d62cc9212d88ed
Marcel <MarcelReinecke@gmx.de>
2018-11-10 13:57:14 +0100

    sounds and first config for licenses

---
+++ config/Config_licence.cpp
@@ -0,0 +1,23 @@
+class Config_licence
+{
+      class Driver_licence_A
+      {
+          DisplayName = "Führerschein Klasse A";
+
+      };
+
+      class Driver_licence_B
+      {
+          DisplayName = "Führerschein Klasse B";
+
+      };
+
+      class Driver_licence_C
+      {
+          DisplayName = "Führerschein Klasse C";
+
+      };
+
+
+
+  };
