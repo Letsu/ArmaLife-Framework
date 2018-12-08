7ada6ea12cbf7b58c54d741a03e72ef0d6aaa26a
Johannes Letus Bindriem <johannes.bindriem@yahoo.de>
2018-12-08 22:38:13 +0100

    fix vehicle sql code

--- db/vehicles.sql
+++ db/vehicles.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS `arma`.`vehicles`  (
   `id` int(6) NOT NULL AUTO_INCREMENT,
   `plate` int(8) NOT NULL,
   `PID` int(18) NOT NULL,
-  `keyOwner` char(255) NOT NULL DEFAULT [],
+  `keyOwner` char(255) NOT NULL,
   `fuel` int(100) NOT NULL DEFAULT 100,
-  `inventory` char(2500) NOT NULL,
-  `available tinyint(2) NOT NULL DEFAULT 0,
+  `inventory` char(2500) NOT NULL DEFAULT [],
+  `available` tinyint(2) NOT NULL DEFAULT 0,
   `alive` tinyint(2) NOT NULL,
   PRIMARY KEY (`plate`)
 );
