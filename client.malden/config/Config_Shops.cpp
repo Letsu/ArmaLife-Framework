2d08558e864b4c667b8e6240a865a82b4565c86a
Johannes Letus Bindriem <johannes.bindriem@yahoo.de>
2019-03-09 14:48:07 +0100

    add Missionsqm and add Police test configs

--- client.malden/config/Config_Shops.cpp
+++ client.malden/config/Config_Shops.cpp
@@ -13,7 +13,40 @@ class Config_Shops {
         DisplayName = "Shop01";
         Condition = "";
         Items[] = {
-            {"arifle_MX_F", "", 1000, 100, ""}, 
+            {"arifle_MX_F", "", 1000, 100, ""},
+            {"NVGoggles", "", 100, 10, ""}
+        };
+    };
+
+    class policeShop {
+        DisplayName = "Polizei Dienst Ausrüstung";
+        Condition = "";
+        Items[] = {
+            {"U_B_GEN_Commander_F", "", 1000, 100, ""},
+            {"U_B_GEN_Soldier_F", "", 1000, 100, ""},
+            {"H_Beret_02", "", 1000, 100, ""},
+            {"H_Cap_police", "", 1000, 100, ""},
+            {"SMG_03C_TR_black", "", 1000, 100, ""},
+            {"50Rnd_570x28_SMG_03", "", 1000, 100, ""},
+            {"arifle_MXC_Black_F", "", 1000, 100, ""},
+            {"30Rnd_65x39_caseless_mag", "", 1000, 100, ""},
+            {"SMG_05_F", "", 1000, 100, ""},
+            {"30Rnd_9x21_Mag_SMG_02", "", 1000, 100, ""},
+            {"optic_Arco_blk_F", "", 1000, 100, ""},
+            {"ACE_fieldDressing", "", 10, 1, ""},
+            {"ACE_elasticBandage", "", 10, 1, ""},
+            {"ACE_quikclot", "", 10, 1, ""},
+            {"ACE_atropine", "", 10, 1, ""},
+            {"ACE_adenosine", "", 10, 1, ""},
+            {"ACE_Banana", "", 10, 1, ""},
+            {"ACE_CableTie", "", 10, 1, ""},
+            {"ACE_bloodIV_250", "", 10, 1, ""},
+            {"ACE_EarPlugs", "", 10, 1, ""},
+            {"ACE_epinephrine", "", 10, 1, ""},
+            {"ACE_packingBandage", "", 10, 1, ""},
+            {"ACE_morphine", "", 10, 1, ""},
+            {"L_Item_WaterBottleFull", "", 10, 1, ""},
+            {"tf_anprc148jem_1", "", 10, 1, ""},
             {"NVGoggles", "", 100, 10, ""}
         };
     };
